0b22ac035e74a48ca694be2da9c89473ac403cfe
ThePrakharRajpali <rprakhar@iitg.ac.in>
2021-01-09 23:39:45 +0530

    added post func. (BETA)

--- popup.js
+++ popup.js
@@ -5,11 +5,15 @@ var clickButton = document.getElementById("clickButton");
 var express = require('express');
 var app = express();
 
+const apiLink = "";
+
 clickButton.addEventListener("click", () => {
     chrome.tabs.query({active:true, lastFocusedWindow:true}, tabs => {
         let url = tabs[0].url;
         toPrint.innerHTML = url;
-        let toSave = {url: url};
 
+        $.post(apiLink, {"url": url}, (data) => {
+            alert("saved bookmark");
+        }, "json");
     });
 });
